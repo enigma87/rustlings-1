2043482410a4cf947ac3ef33522f1369c728aad9
kishore kailainathan <kishoregok@gmail.com>
2023-12-19 21:06:11 -0500

    primitives

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,16 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(num_apples:i32) -> i32 {
+    if num_apples > 40 {
+        return num_apples
+    }
+    else {
+        return num_apples * 2
+    }
+}
 
 // Don't modify this function!
 #[test]
